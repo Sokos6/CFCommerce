2dd13ca7a43a74623965a9f5292bc8430e1d0965
Sokos6 <will.sokolowski@gmail.com>
2020-07-26 18:20:49 -0500

    category nav

---
+++ src/shop/CategoryNavigation.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class CategoryNavigation extends Component {
+  render() {
+    return (
+      <>
+        <Link className='btn btn-secondary btn-block' to={this.props.baseUrl}>
+          All
+        </Link>
+        {this.props.categories &&
+          this.props.categories.map((cat) => (
+            <Link
+              className='btn btn-secondary btn-block'
+              key={cat}
+              to={`${this.props.baseUrl}/${cat.toLowerCase()}`}
+            >
+              {cat}
+            </Link>
+          ))}
+      </>
+    );
+  }
+}
--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class ProductList extends Component {
   render() {
-    if (this.props.products == null || this.props.products.length === 0 ) {
-      return <h5 className='p-2'>No Products</h5>
+    if (this.props.products == null || this.props.products.length === 0) {
+      return <h5 className="p-2">No Products</h5>;
     }
-    return this.props.products.map(p =>
-      <div className='card m-1 p-1 bg-light' key={ p.id} >
+    return this.props.products.map((p) => (
+      <div className="card m-1 p-1 bg-light" key={p.id}>
         <h4>
-          { p.name }
-          <span className='badge badge-pill badge-primary float-right'>
-          ${ p.price.toFixed(2) }
+          {p.name}
+          <span className="badge badge-pill badge-primary float-right">
+            ${p.price.toFixed(2)}
           </span>
-          </h4>
-          <div className='card-taxt bg-white p-1'>
-          { p.description }
-       </div>
+        </h4>
+        <div className="card-taxt bg-white p-1">{p.description}</div>
       </div>
-      )
+    ));
   }
 }
