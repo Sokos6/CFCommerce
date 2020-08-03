a79657714eb21d4eaffada194ab1a5212a102b30
Sokos6 <will.sokolowski@gmail.com>
2020-08-03 14:06:14 -0500

    update Data Store

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux'
-import { ShopReducer } from './ShopReducer'
+import { createStore } from 'redux';
+import { ShopReducer } from './ShopReducer';
+import { CartReducer } from './CartReducer';
+import { CommonReducer } from './CommonReducer';
 
-export const CFCommerceDataStore = createStore(ShopReducer);
+export const CFCommerceDataStore = createStore(
+  CommonReducer(ShopReducer, CartReducer)
+);
