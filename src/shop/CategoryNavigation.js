4b5fd268cf4ae7fd49598551ae260f990ae6ec74
Sokos6 <will.sokolowski@gmail.com>
2020-08-02 23:59:18 -0500

    toggle link category nav update

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ToggleLink } from '../ToggleLink';
 
 export class CategoryNavigation extends Component {
   render() {
     return (
       <>
-        <Link className='btn btn-secondary btn-block' to={this.props.baseUrl}>
-          All
-        </Link>
+        <ToggleLink to={this.props.baseUrl} exact={true}></ToggleLink>
         {this.props.categories &&
           this.props.categories.map((cat) => (
-            <Link
-              className='btn btn-secondary btn-block'
+            <ToggleLink
               key={cat}
               to={`${this.props.baseUrl}/${cat.toLowerCase()}`}
             >
               {cat}
-            </Link>
+            </ToggleLink>
           ))}
       </>
     );
