34d1c2641c5c5471537b5d511a583a95a0d7b49a
Sokos6 <will.sokolowski@gmail.com>
2020-08-03 13:32:10 -0500

    cart action creators

--- src/data/Types.js
+++ src/data/Types.js
@@ -4,6 +4,7 @@ export const DataTypes = {
 };
 export const ActionTypes = {
   DATA_LOAD: 'data_load',
+  CART_ADD: 'cart_add',
   CART_UPDATE: 'cart_update',
   CART_REMOVE: 'cart_delete',
   CART_CLEAR: 'cart_clear',
