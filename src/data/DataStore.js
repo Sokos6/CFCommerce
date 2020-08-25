3c56dcbbb644228e4efc1279e3ec9f0675077bf8
Sokos6 <will.sokolowski@gmail.com>
2020-08-25 00:06:09 -0500

    middleware

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { ShopReducer } from './ShopReducer';
 import { CartReducer } from './CartReducer';
 import { CommonReducer } from './CommonReducer';
-export const CFCommerceDataStore = createStore(
-  CommonReducer(ShopReducer, CartReducer)
+import { asyncActions } from './AsnycMiddleware';
+
+export const SportsStoreDataStore = createStore(
+  CommonReducer(ShopReducer, CartReducer),
+  applyMiddleware(asyncActions)
 );
