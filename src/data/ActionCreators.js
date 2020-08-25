6333900ae5e6c0df0b1a4ed71a97823946e70e5c
Sokos6 <will.sokolowski@gmail.com>
2020-08-25 18:34:46 -0500

    changing action

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -4,9 +4,14 @@ import { RestDataSource } from './RestDataSource';
 
 const dataSource = new RestDataSource();
 
-export const loadData = (dataType) => ({
+export const loadData = (dataType, params) => ({
   type: ActionTypes.DATA_LOAD,
   payload: dataSource
-    .GetData(dataType)
-    .then((response) => ({ dataType, data: response.data })),
+    .GetData(dataType, params)
+    .then((response) => ({
+      dataType,
+      data: response.data,
+      total: Number(response.headers['x-total-count']),
+      params,
+    })),
 });
