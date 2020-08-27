41cfe4411afc7f18a3c42d06444e589afbc3d04f
Sokos6 <will.sokolowski@gmail.com>
2020-08-26 23:23:51 -0500

    pagination controls update

--- src/PaginationControls.js
+++ src/PaginationControls.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import { PaginationButtons } from './PaginationButtons';
-
 export class PaginationControls extends Component {
   constructor(props) {
     super(props);
     this.pageSizes = this.props.sizes || [5, 10, 25, 100];
     this.sortKeys = this.props.keys || ['Name', 'Price'];
   }
-
-  handlePageSize = (ev) => {
+  handlePageSizeChange = (ev) => {
     this.props.setPageSize(ev.target.value);
   };
-
   handleSortPropertyChange = (ev) => {
     this.props.setSortProperty(ev.target.value);
   };
-
   render() {
     return (
       <div className='m-2'>
@@ -26,7 +22,30 @@ export class PaginationControls extends Component {
             navigate={this.props.navigateToPage}
           />
         </div>
-        
+        <div className='form-inline justify-content-center'>
+          <select
+            className='form-control'
+            onChange={this.handlePageSizeChange}
+            value={this.props.pageSize || this.pageSizes[0]}
+          >
+            {this.pageSizes.map((s) => (
+              <option value={s} key={s}>
+                {s} per page
+              </option>
+            ))}
+          </select>
+          <select
+            className='form-control'
+            onChange={this.handleSortPropertyChange}
+            value={this.props.sortKey || this.sortKeys[0]}
+          >
+            {this.sortKeys.map((k) => (
+              <option value={k.toLowerCase()} key={k}>
+                Sort By {k}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     );
   }
